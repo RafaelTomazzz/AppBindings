65fb6165588275660857ebcc48c549c6c3eb936b
Etec Prof. Horácio Augusto da Silveira <rafaeltomaz2701@gmail.com>
2023-08-18 22:40:20 -0300

    first commit

---
+++ AppBindings/AppBindings/ViewModels/UsuarioViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppBindings.ViewModels
+{
+    public class UsuarioViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string name = string.Empty;//CTRL + R, E
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == null)
+                    return;
+
+                name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+
+        public string DisplayName => $"Nome digitado: {Name}";
+
+        public async Task CountCharacters()
+        {
+            string nameLegth =
+                string.Format("Seu nome tem {0} letras", name.Length);
+
+            await Application.Current
+                .MainPage.DisplayAlert("Informação", nameLegth, "Ok");
+        }
+
+        public ICommand CountCommand { get; }
+
+        public UsuarioViewModel() 
+        {
+            CountCommand =  new Command(async () => await CountCharacters());
+        }
+
+    }
+
+}
---
+++ AppBindings/AppBindings/Views/UsuarioView.xaml.cs
@@ -0,0 +1,14 @@
+using AppBindings.ViewModels;
+
+namespace AppBindings.Views;
+
+public partial class UsuarioView : ContentPage
+{
+	public UsuarioView()
+	{
+		InitializeComponent();
+		BindingContext = new UsuarioViewModel();
+	}
+
+	
+}
